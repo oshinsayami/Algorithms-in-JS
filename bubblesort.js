bdaccdd9b5476ece2b3793df5a4e774295dd2d3a
Simran <mdrsimran@gmail.com>
2021-06-10 12:29:49 +0545

    JS ES6

--- bubblesort.js
+++ bubblesort.js
@@ -3,9 +3,10 @@ function bubbleSort(array) {
     for (let i = 0; i < array.length-1; i++){
         for (let j = 0; j < array.length - 1-i; j++){
             if (array[j] > array[j + 1]) {
-                const temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+                [array[j], array[j+1]] = [array[j+1], array[j]] //ES6
+                // const temp = array[j];
+                // array[j] = array[j + 1];
+                // array[j + 1] = temp;
             }
         }
     }
