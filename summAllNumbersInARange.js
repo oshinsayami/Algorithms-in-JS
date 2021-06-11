f38457b2d808b820de772dadeb01a62725e47b92
Simran <mdrsimran@gmail.com>
2021-06-11 18:20:21 +0545

    code using ternary operator

--- summAllNumbersInARange.js
+++ summAllNumbersInARange.js
@@ -1,12 +1,16 @@
 function sumAll(arr) {
-    let min, max;
-    if (arr[0] > arr[1]) {
-        max = arr[0];
-        min = arr[1];
-    } else {
-        max = arr[1];
-        min=arr[0];
-    }
+    //using ternary operator
+    const min = arr[0] < arr[1] ? arr[0] : arr[1];
+    const max = arr[0] > arr[1] ? arr[0] : arr[1];
+
+    // let min, max;
+    // if (arr[0] > arr[1]) {
+    //     max = arr[0];
+    //     min = arr[1];
+    // } else {
+    //     max = arr[1];
+    //     min=arr[0];
+    // }
 
     let sum = 0;
     for (let i = min; i <= max; i++){
@@ -15,4 +19,4 @@ function sumAll(arr) {
     return sum;
 }
 
-console.log(sumAll([4, 1]));
+console.log(sumAll([4, 4]));
