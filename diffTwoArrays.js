5e80cfd52d595354797c915dfad324148c69be31
Simran <mdrsimran@gmail.com>
2021-06-13 14:43:47 +0545

    get the union first

---
+++ diffTwoArrays.js
@@ -0,0 +1,16 @@
+function diffArray(arr1, arr2) {
+    const union = [];
+    for (let i = 0; i < arr1.length; i++){
+        if (!union.includes(arr1[i])) {
+            union.push(arr1[i]);
+        }
+    }
+    for (let i = 0; i < arr2.length; i++){
+        if (!union.includes(arr2[i])) {
+            union.push(arr2[i]);
+        }
+    }
+    console.log(union);
+}
+
+diffArray([1,2,3,4],[5,6,7,8])
