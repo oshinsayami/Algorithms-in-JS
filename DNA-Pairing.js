395b8b94bac290225aa9b079d355f06c684b9466
Simran <mdrsimran@gmail.com>
2021-06-19 13:43:14 +0545

    reactor to object

--- DNA-Pairing.js
+++ DNA-Pairing.js
@@ -1,15 +1,22 @@
+const dnaDict = {
+    A: 'T',
+    T: 'A',
+    C: 'G',
+    G: 'C'
+}
+
 function pairElement(str) {
 
     let output = [];
     for (const c of str) {
         if (c === 'A') {
-            output.push([c, 'T']);
+            output.push([c, dnaDict[c]]);
         } else if (c === 'T') {
-            output.push([c, 'A']);
+            output.push([c, dnaDict[c]]);
         } else if (c === 'C') {
-            output.push([c, 'G']);
+            output.push([c, dnaDict[c]]);
         } else if(c === 'G') {
-            output.push([c, 'C']);
+            output.push([c, dnaDict[c]]);
         }
     }
     return output;
