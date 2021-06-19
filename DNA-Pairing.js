a4af2912e33a6b2b7555731fb6dcfd84a4d1d67f
Simran <mdrsimran@gmail.com>
2021-06-19 13:44:24 +0545

    no need of forloop

--- DNA-Pairing.js
+++ DNA-Pairing.js
@@ -9,15 +9,7 @@ function pairElement(str) {
 
     let output = [];
     for (const c of str) {
-        if (c === 'A') {
-            output.push([c, dnaDict[c]]);
-        } else if (c === 'T') {
-            output.push([c, dnaDict[c]]);
-        } else if (c === 'C') {
-            output.push([c, dnaDict[c]]);
-        } else if(c === 'G') {
-            output.push([c, dnaDict[c]]);
-        }
+        output.push([c, dnaDict[c]]);
     }
     return output;
 
