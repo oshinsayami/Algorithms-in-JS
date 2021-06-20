2b4cc5c12193efee13f107560923769a15bde6cc
Simran <mdrsimran@gmail.com>
2021-06-20 11:41:09 +0545

    using Set ES6 datatype

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -1,9 +1,14 @@
 let a = [1, 2, 4, 5, 3, 2, 1, 6];
-obj = {};
-for (let i of a) {
-    obj[i] = true;
-}
+// obj = {};
+// for (let i of a) {
+//     obj[i] = true;
+// }
 
-let b = Object.keys(obj);
+// let b = Object.keys(obj);
 
-console.log(b);
+// console.log(b);
+
+
+//using Set
+
+console.log([...new Set(a)]);
