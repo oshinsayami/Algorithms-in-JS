b4eba8e0d94a92c0bf615c82cee990970c4f75b4
Simran <mdrsimran@gmail.com>
2021-06-21 12:24:49 +0545

    complete code

---
+++ missingLetter.js
@@ -0,0 +1,20 @@
+function fearNotLEtter(str) {
+    let output = '';
+
+    for (let i = 0; i < str.length - 1; i++){
+        const currentCode = str.charCodeAt(i);
+        const nextCode = str.charCodeAt(i + 1);
+
+        if (nextCode - currentCode !== 1) {
+            output = String.fromCharCode(currentCode + 1);
+            break;
+        }
+    }
+    if (output === '') {
+        return undefined;
+    }
+    return output; 
+}
+
+console.log(fearNotLEtter("abce"));
+console.log(fearNotLEtter("abcdefghijklmnopqrstuvwxyz"));
