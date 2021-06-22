0696e519e85cf89d2f9404dd9ecb0037e5c70ec6
Simran <mdrsimran@gmail.com>
2021-06-22 13:06:28 +0545

    using Set object to remove duplicate elements in an array

--- sortedUnion.js
+++ sortedUnion.js
@@ -4,13 +4,17 @@ function uniteUnique(arr) {
 
     const newArr = arrOfArrs.flat();
 
-    const output = [];
-    for (const num of newArr) {
-        if (!output.includes(num)) {
-            output.push(num);
-        }
-    }
-    return output;
+    const uniqArr = [...new Set(newArr)];
+
+    return uniqArr;
+
+    // const output = [];
+    // for (const num of newArr) {
+    //     if (!output.includes(num)) {
+    //         output.push(num);
+    //     }
+    // }
+    // return output;
 }
 
 console.log(uniteUnique([1, 3, 4], [5, 2, 1, 4], [2, 1]));
