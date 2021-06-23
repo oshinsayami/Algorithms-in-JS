230a20b38da1fb8842440ef073e2994a0bacf995
Simran <mdrsimran@gmail.com>
2021-06-23 16:32:28 +0545

    full code

---
+++ convertHTMLEntities.js
@@ -0,0 +1,21 @@
+function convertHTML(str) {
+    let output = '';
+
+    for (const char of str) {
+        if (char === '&') {
+            output += '&amp';
+        } else if (char === '<') {
+            output += '&lt';
+        } else if (char === '>') {
+            output += '&gt';
+        } else if (char === '"' {
+            output += '&quot';
+        } else if (char === "'") {
+            output += '&apos';
+        } else {
+            output += char;
+        }
+    }
+}
+
+console.log(convertHTML("Dolce & Gabbana"));
