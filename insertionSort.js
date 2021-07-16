ecf1582196c10c86a275404ac9b1b04fb54fb61c
Simran <mdrsimran@gmail.com>
2021-07-16 14:02:42 -0400

    fixed a typo

--- insertionSort.js
+++ insertionSort.js
@@ -1,6 +1,6 @@
 function insertionSort(arr) {
 
-    const array = arr.alice();
+    const array = arr.slice();
     for (let i = 1; i < array.length; i++){
         for (let j = i; j > 0; j--){
             if (array[j] < array[j - 1]) {
