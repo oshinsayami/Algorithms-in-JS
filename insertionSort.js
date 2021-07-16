029b35332f764f131bf00b108d7f673f125b839b
Simran <mdrsimran@gmail.com>
2021-07-16 14:01:03 -0400

    complete code

---
+++ insertionSort.js
@@ -0,0 +1,14 @@
+function insertionSort(array) {
+    for (let i = 1; i < array.length; i++){
+        for (let j = i; j > 0; j--){
+            if (array[j] < array[j - 1]) {
+                [array[j], array[j-1]] = [array[j - 1],array[j]];
+            } else {
+                break;
+            }
+        }
+    }
+    return array;
+}
+
+console.log(insertionSort([1, 4, 2, 8, 97, 45, 34, 67, 987, 678, 876, 54, 43, 32, 2]));
