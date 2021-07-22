f382c34b674ff7dbacf6417e1db5c5df2c40a4b3
Simran <mdrsimran@gmail.com>
2021-07-22 10:53:28 -0400

    full code

--- quickSort.js
+++ quickSort.js
@@ -1,3 +1,26 @@
 function quickSort(array) {
-    
+    if (array.length === 1) {
+        return array;
+    }
+    const pivot = array[array.length - 1];
+    const leftA = [];
+    const rightA = [];
+    for (let i = 0; i < array.length - 1; i++){
+        if (array[i] < pivot) {
+            leftA.push(array[i]);
+        } else {
+            rightA.push(array[i]);
+        }
+    }
+
+    if (leftA.length > 0 && rightA.length > 0) {
+        return [...quickSort(leftA), pivot, ...quickSort(rightA)];
+    } else if (leftA.length > 0) {
+        return [...quickSort(leftA), pivot];
+    } else {
+        return [pivot, ...quickSort(rightA)];
+    }
 }
+
+const arr = [2, 6, 3, 7, 4, 8];
+console.log(quickSort(arr));
